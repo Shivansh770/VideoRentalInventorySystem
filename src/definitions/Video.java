16381003db36d3e105ff25831d9ce939242872c8
Shivansh Gupta <shivansh.gupta_cs19@gla.ac.in>
2020-08-19 20:34:19 +0530

    add Video.java

---
+++ src/definitions/Video.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Shivansh Gupta (Shivansh770)
+ *   Date: 19-08-2020
+ *   Time: 20:29
+ *   File: Video.java
+ */
+
+package definitions;
+
+public class Video {
+}
